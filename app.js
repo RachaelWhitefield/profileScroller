3369a62ce669776a105ef271885d9a48f0df6857
Fabulous_Rachael <rachael.whitefield@gmail.com>
2020-02-07 15:35:25 -0700

    updated appjs to call to api for random users always instead of hard coded data

--- app.js
+++ app.js
@@ -1,31 +1,12 @@
-const data = [
-    {
-        name: "John Doe",
-        age: 32,
-        gender: "male",
-        lookingfor: "female",
-        location: "Boston MA",
-        image: "https://randomuser.me/api/portraits/men/82.jpg"
-    },
-    {
-        name: "Jenn Smith",
-        age: 28,
-        gender: "female",
-        lookingfor: "male",
-        location: "Miami FL",
-        image: "https://randomuser.me/api/portraits/women/82.jpg"
-    },
-    {
-        name: "William Johnson",
-        age: 38,
-        gender: "male",
-        lookingfor: "female",
-        location: "Denver CO",
-        image: "https://randomuser.me/api/portraits/men/83.jpg"
-    }
-];
-
-const profiles = profileIterator(data);
+$.ajax({
+    url: 'https://randomuser.me/api/?nat=us',
+    dataType: 'json',
+    success: function(data) {
+    //   console.log(data.results);
+    //   console.log("2", data.results[0].name.first)
+      const results = data.results;
+      const profiles = profileIterator(results);
+        
 
 // Call first profile
 nextProfile();
@@ -40,15 +21,15 @@ function nextProfile() {
     if(currentProfile !== undefined) {
         document.getElementById("profileDisplay").innerHTML = 
             `<ul class="list-group">
-                <li class="list-group-item">Name: ${currentProfile.name}</li>
-                <li class="list-group-item">Age: ${currentProfile.age}</li>
-                <li class="list-group-item">Location: ${currentProfile.location}</li>
-                <li class="list-group-item">Preference: ${currentProfile.gender} looking for ${currentProfile.lookingfor}</li>
+                <li class="list-group-item">Name: ${results[0].name.first}</li>
+                <li class="list-group-item">Age: ${results[0].dob.age}</li>
+                <li class="list-group-item">City: ${results[0].location.city}</li>
+                <li class="list-group-item">State: ${results[0].location.state}</li>
             </ul>`
         ;
 
         document.getElementById("imageDisplay").innerHTML = 
-        `<img src="${currentProfile.image}">`;
+        `<img src="${results[0].picture.large}">`;
     } else {
         // no more profiles
         window.location.reload();
@@ -67,3 +48,5 @@ function profileIterator(profiles) {
         }
     };
 }
+}
+});
